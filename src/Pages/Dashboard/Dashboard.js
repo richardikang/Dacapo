60304ef8e1297fbf76172a28c9ea283eb6969db3
richardikang <76178368+richardikang@users.noreply.github.com>
2025-05-11 18:48:16 +0200

    Update Dashboard.js

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,1 +1,16 @@
 //task: group anagram (https://leetcode.com/problems/group-anagrams/description/)
+//Once a freelancer successfully completes the task, they would potentially be provided an assignment by the client before they are officially hired
+//Sample assignment:
+//<h1>Software engineer needed to create a dynamic word puzzle</h1>
+//<li>timed word search puzzle</li>
+//<li>letters move every 10 second</li>
+ //<ul>horizontally</ul>
+ //<ul>vertically</ul>
+ //<ul>diagonally</ul>
+//<li>user has to score as many points as possible before the timer runs out</li>
+//<p1>Display includes:</p1>
+//<li>timer</li>
+//<li>cumulative points</li>
+//<li>warning message during the remaining 10 seconds before the game ends</li>
+//<li>final score</li>
+//<li>option to continue or quit once the game ends</li>
